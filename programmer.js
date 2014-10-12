5a602b4ad162a18580f1908db46e020e3d533183
= <=>
2014-10-12 06:14:47 +0200

    Added proxy and programmer to manage proxy ports

--- programmer.js
+++ programmer.js
@@ -1,11 +1,15 @@
 var net  = require('net');
-var proxy_ip    = process.argv[2]; 
-var remote_ip   = process.argv[3];
-var remote_port = process.argv[4];
+var proxy_ip        = process.argv[2]; 
+var proxy_port      = process.argv[3]; // Puerto que va a levantarse
+var operation       = process.argv[4];
+var new_remote_ip   = process.argv[5];
+var new_remote_port = process.argv[6];
+var operation 
  
-var client = net.connect({port:8001,host:proxy_ip},function(){
-	console.log('programmer connected');
-	client.write(JSON.stringify({"remote_ip":remote_ip,"remote_port":remote_port}));
+var client = net.connect({port:8000,host:proxy_ip},function(){
+    console.log('programmer connected');
+    client.write(JSON.stringify({"op":operation,"inPort":proxy_port,"remote":{"ip":new_remote_ip,"port":new_remote_port}}));
+    
 });
 
 client.on('data',function(data){
